fe8d86260132d7863d88e2f9aec2930dfa36a4b5
sungjinHong <bbabbo33@gmail.com>
2018-11-26 11:05:33 +0900

    example commit

--- src/kr/controller/member/AddUserForm_Controller.java
+++ src/kr/controller/member/AddUserForm_Controller.java
@@ -20,6 +20,7 @@ public class AddUserForm_Controller implements Controller {
 	@Override
 	public String handRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String token = request.getParameter("token");
+		System.out.println("!!!");
 
 		if (request.getParameter("is_naver") != null) { // 네이버 로그인한경우
 			request.setAttribute("is_naver", 1);
