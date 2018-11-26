7bce6e27233247c0fb2e28f2ed20666aa7f05942
choichangkyu <cck742@naver.com>
2018-11-26 14:34:21 +0900

    cancel apply

--- src/kr/controller/apply/CancelApply_Controller.java
+++ src/kr/controller/apply/CancelApply_Controller.java
@@ -1,4 +1,4 @@
-package kr.controller.myPage;
+package kr.controller.apply;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,18 +7,20 @@ import kr.controller.Controller;
 import kr.dao.ApplyDAO;
 import kr.vo.ApplyVO;
 
-public class ReservationListController implements Controller {
-
+public class CancelApply_Controller implements Controller{
 	@Override
 	public String handRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		
-		int no = Integer.parseInt(request.getParameter("no"));
-		ApplyVO apply = new ApplyVO();
+		request.setCharacterEncoding("utf-8");
 		ApplyDAO dao = new ApplyDAO();
 		
+		int no = Integer.parseInt(request.getParameter("no"));
 		
-		return "/page/myPage/reservationList.jsp";
+		ApplyVO apply = dao.selectByNo(no);
+		int result = dao.cancelApply(apply);
+		
+		request.setAttribute("cnt", result);
+	
+		return "/jsp/apply/cancel_apply.jsp";
 	}
 
 }
